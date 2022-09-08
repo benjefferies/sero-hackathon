145a1ce55d0396ab43cdbdb86ed01e581fb326de
Ben Jefferies <benjefferies@echosoft.uk>
2022-09-08 11:27:35 +0100

    App

--- sero-hackathon-app/src/App.tsx
+++ sero-hackathon-app/src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { RankingCard } from "./components/ranking";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <RankingCard
+        co2={200}
+        cost={20}
+        dailyRank={1}
+        leagueRank={1}
+        locality={"Chessel Drive, BS34"}
+        propertyId="1"
+      />
     </div>
   );
 }
--- sero-hackathon-app/src/components/ranking.tsx
+++ sero-hackathon-app/src/components/ranking.tsx
@@ -1,18 +1,19 @@
 import { Ranking } from "../types/ranking";
+import "./ranking.css";
 
 export interface RankingProps extends Ranking {}
 
-function RankingCard(props: RankingProps) {
+export function RankingCard(props: RankingProps) {
   return (
     <div className="card">
       <div className="card-title">
-        <p className="rank">{props.leagueRank}</p>
+        <p className="rank">#{props.leagueRank}</p>
         <p className="rank-locality">{props.locality}</p>
       </div>
       <div className="card-body">
-        <p className="card-co2">{props.co2}</p>
-        <p className="card-cost">{props.cost}</p>
-        <p className="card-daily-rank">{props.dailyRank}</p>
+        <p className="card-co2">CO2: {props.co2}kg</p>
+        <p className="card-cost">Cost: £{props.cost}</p>
+        <p className="card-daily-rank">Daily rank: {props.dailyRank}</p>
         <button
           className="card-view"
           onClick={() => console.log(props.propertyId)}
